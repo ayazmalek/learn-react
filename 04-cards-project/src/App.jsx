e3a94b6e1347a7630adbe1132e7f8e21cac8c5ab
Ayaz Malek <ayazmalek188@gmail.com>
2026-08-18 18:58:36 +0530

    some changes

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { Bookmark } from "lucide-react";
 
 const App = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="parent">
+        <div className="card">
+<div>
+            <div className="top">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi6U-KSnY_r4hsTsVxDuaaM1suHkBJRHcyaf65xF5PJQ&s=10"
+              alt=""
+            />
+            <button>
+              save <Bookmark size={12} />{" "}
+            </button>
+          </div>
+          <div className="center">
+            <h3>
+              Amazon <span>5 days ago</span>
+            </h3>
+            <h2>Senior UI/UX designer</h2>
+            <div className="tag">
+              <h4>Part time</h4>
+              <h4>Senior level</h4>
+            </div>
+          </div>
+</div>
+          <div className="bottom">
+            <div>
+              <div>$120/hr</div>
+              <p>Mumbai ,inida </p>
+            </div>
+            <button>  Apply Now</button>
+          </div>
 
-export default App
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
