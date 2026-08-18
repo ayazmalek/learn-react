dc7ca797640438105092a8e7f23d6d073be1f461
Ayaz Malek <ayazmalek188@gmail.com>
2026-08-19 00:42:35 +0530

    small project work done

--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -1,45 +1,31 @@
-import React from "react";
-import { Bookmark } from "lucide-react";
+import { Bookmark } from 'lucide-react'
+const Card = (props) => {
 
-const Cards = () => {
-  return (
-    <div className="card">
-      <div>
-        <div className="top">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi6U-KSnY_r4hsTsVxDuaaM1suHkBJRHcyaf65xF5PJQ&s=10"
-            alt=""
-          />
-
-          <button>
-            Save <Bookmark size={12} />
-          </button>
+    return (
+        <div className="card">
+            <div>
+                <div className="top">
+                    <img src={props.brandLogo} alt="nachioooooo" />
+                    <button>Save <Bookmark size={10} /> </button>
+                </div>
+                <div className="center">
+                    <h3>{props.company} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}</h2>
+                    <div className='tag'>
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
         </div>
+    )
+}
 
-        <div className="center">
-          <h3>
-            Amazon <span>5 days ago</span>
-          </h3>
-
-          <h2>Senior UI/UX Designer</h2>
-
-          <div className="tag">
-            <h4>Part time</h4>
-            <h4>Senior level</h4>
-          </div>
-        </div>
-      </div>
-
-      <div className="bottom">
-        <div>
-          <div>$120/hr</div>
-          <p>Mumbai, India</p>
-        </div>
-
-        <button>Apply Now</button>
-      </div>
-    </div>
-  );
-};
-
-export default Cards;
+export default Card
