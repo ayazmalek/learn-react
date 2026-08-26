6d11c89e7a15eb53c83f52b51be0f5578788ddf1
Ayaz Malek <ayazmalek188@gmail.com>
2026-08-26 22:57:16 +0530

    axios / fetch api concept understanding

--- 15-apicalling/src/App.jsx
+++ 15-apicalling/src/App.jsx
@@ -1,17 +1,29 @@
 import axios from 'axios'
+import { useState } from 'react'
 
 const App = () => {
-
+    const [data, setData] = useState([])
   const getData =async()=>{
-  const response =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
-const data =await response.json();
-console.log(data);
 
+//   const response =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+// const data =await response.json();
+// console.log(data);
+
+    const response =await axios.get('https://picsum.photos/v2/list')
+   
+    
+
+    setData(response.data)
 
   }
   return (
     <div>
       <button onClick={getData}>click</button>
+      <div>
+          {data.map(function(elem , idx){
+            return <h3> hello ,{elem.author} {idx} </h3>
+          })}
+      </div>
     </div>
   )
 }
