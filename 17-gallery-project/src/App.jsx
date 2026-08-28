cf681a7ce5930979c567aa06745af4b7f661287d
Ayaz Malek <ayazmalek188@gmail.com>
2026-08-28 12:27:16 +0530

    porject initialize

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,9 +1,22 @@
+import { ImageOff } from 'lucide-react'
 import React from 'react'
-
+import axios from 'axios'
 const App = () => {
+
+  const getData = async () => {
+  const response = await axios(
+    "https://picsum.photos/v2/list?page=2&limit=100"
+  );
+
+  const data = response.data;
+
+  console.log(data);
+}; 
   return (
     <div className='bg-black h-screen text-white  '>
-      hello
+    <button onClick={getData}
+    className='bg-green-600 px-5 py-2 text-white rounded  mb-3 active:scale-95'> get data</button>
+
     </div>
   )
 }
