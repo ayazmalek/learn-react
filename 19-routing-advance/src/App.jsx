b7e6f891706be212071bed92a1021892ce856381
Ayaz Malek <ayazmalek188@gmail.com>
2026-08-31 22:27:41 +0530

    nested routing

--- 19-routing-advance/src/App.jsx
+++ 19-routing-advance/src/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import  Home from './pages/Home'
+import { Routes , Route } from 'react-router-dom'
+import About from './pages/About'
+import Product from './pages/Product'
+import Notfound from './pages/Notfound'
+import Men from './pages/Men'
+import Women from './pages/Women'
+import Kids from './pages/Kids'
 
 const App = () => {
   return (
     <div className='h-screen bg-black text-white '>
      <Navbar />
+  <Routes>
+    <Route path='/' element={<Home />}/>
+<Route path='/about' element={<About />} />
+<Route  path='*' element={<Notfound/>} />
+<Route path='/product' element={<Product />} >
+
+<Route path='men' element={<Men />} />
+<Route path='women' element={<Women />} />
+<Route path='kids' element={<Kids />} />
+</Route>
+
+<Route path='/product/men' element={<Men />} />
+<Route path='/product/women' element={<Women />} />
+<Route path='/product/kids' element={<Kids />} />
+
+  </Routes>
      <Footer/> 
     </div>
   )
--- 19-routing-advance/src/components/Navbar.jsx
+++ 19-routing-advance/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='flex  bg-gray-800 items-center px-8 py-4 justify-between'>
-      <h2 className='text-xl font-bold'>Bingo</h2>
-<div className='flex gap-8'>
-       <a to='/'>Home</a>
-                <a to='/about'>About</a>
-                <a to='/contact'>Contact</a>
-                <a to='/product'>Product</a>
-    </div>
-    </div>
-  )
-}
+    <div className="flex bg-gray-800 items-center px-8 py-4 justify-between">
+      <h2 className="text-xl font-bold">Bingo</h2>
 
-export default Navbar
+      <div className="flex gap-8">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/product">Product</Link>
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
