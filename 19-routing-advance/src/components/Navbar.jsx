b6965361cf9806375cf59861896ef34b449bdb42
Ayaz Malek <ayazmalek188@gmail.com>
2026-08-31 23:12:52 +0530

    dynamic routing

--- 19-routing-advance/src/components/Navbar.jsx
+++ 19-routing-advance/src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="flex bg-gray-800 items-center px-8 py-4 justify-between">
-      <h2 className="text-xl font-bold">Bingo</h2>
-
-      <div className="flex gap-8">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/product">Product</Link>
-
-      </div>
+    <div className='flex py-4 items-center px-8 bg-cyan-900 justify-between'>
+        <h2 className='text-2xl font-bold'>Bingo</h2>
+        <div className='flex gap-10'>
+          <Link className='text-lg font-medium' to='/'>Home</Link>
+          <Link className='text-lg font-medium' to='/about'>About</Link>
+          <Link className='text-lg font-medium' to='/courses'>Courses</Link>
+          <Link className='text-lg font-medium' to='/product'>Product</Link>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
