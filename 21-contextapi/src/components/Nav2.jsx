12aceda75ce9a2cd237a10e95a251b86f0f4a046
Ayaz Malek <ayazmalek188@gmail.com>
2026-09-01 23:32:24 +0530

    context api

---
+++ 21-contextapi/src/components/Nav2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav2 = () => {
+  return (
+    <div className='nav2'>
+      <h4>home</h4>
+            <h4>contact</h4>
+      <h4>about</h4>
+      <h4>service</h4>
+
+    </div>
+  )
+}
+
+export default Nav2
---
+++ 21-contextapi/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav2 from './Nav2'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <h2>marvel</h2>
+      <Nav2 />
+    </div>
+  )
+}
+
+export default Navbar
